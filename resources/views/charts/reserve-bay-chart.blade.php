910a2e3194762c8f84afca2e53d7931e7fa83cb7
Nafiz Mansor <nafizmansor@gmail.com>
2024-09-29 07:41:08 +0800

    fix the setup API.

--- resources/views/charts/reserve-bay-chart.blade.php
+++ resources/views/charts/reserve-bay-chart.blade.php
@@ -2,7 +2,7 @@
     <div class="card mb-4">
         <div class="card-header">
             <i class="fas fa-chart-bar me-1"></i>
-            Compound Transaction Chart
+            Reserve Bay Chart
         </div>
         <div class="card-body"><canvas id="reserveBayChart" width="100%" height="40"></canvas>
         </div>
