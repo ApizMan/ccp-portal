ba6a06a6dd2b2f6897dfe35cd3f3a9e1b1095865
Nafiz Mansor <nafizmansor@gmail.com>
2024-09-28 14:32:43 +0800

    Done fix the constant.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +19,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(Request $request): void
     {
-        //
+        URL::forceRootUrl($request->root());
     }
 }
